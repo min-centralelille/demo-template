6ee4468338680e5fd78774011645262f9d4a4100
tb59 <thomas.bourdeaudhuy@gmail.com>
2026-09-20 12:27:16 +0000

    etat avant format

--- README.md
+++ README.md
@@ -1,2 +1,28 @@
 # demo-template
-Création d'un dépôt qui servira de template
+
+## Objectifs
+
+À l'issue de ce tutoriel, vous devez savoir :
+
+- écrire une fonction Python ;
+- utiliser des listes ;
+- gérer une erreur avec une exception ;
+- obtenir de l'aide sur le projet ; 
+- vérifier la qualité du code avec `ruff` et l'améliorer ;
+- produire la documentation de votre code avec `pdoc` et l'afficher ;
+- exécuter des tests avec `pytest` ;
+- mettre en place des jeux de tests produisant des courbes ; 
+- automatiser ces traitements à l'aide de la commande `make` ; 
+
+---
+
+## Exercice 1 — Calculer une moyenne
+
+On souhaite écrire une fonction permettant de calculer
+la moyenne des valeurs contenues dans une liste.
+
+La fonction doit avoir la forme :
+
+```python
+def moyenne(valeurs):
+    ...
---
+++ moyenne.py
@@ -0,0 +1,36 @@
+"""Ce module fournit une fonction pour calculer une moyenne de valeurs"""
+
+#### NOTES ####
+# Ce fichier contient du code à compléter
+
+from rich.markdown import Markdown
+from rich.console import Console
+
+def moyenne(valeurs):
+    #docstrings en markdown
+    """
+    # Calcule la moyenne des valeurs.
+
+    **Paramètres**
+    - `valeurs` : valeurs dont on veut calculer la moyenne
+
+    **Retour**
+    - la moyenne des valeurs
+
+    **Exceptions**
+    - `ValueError` : si la liste est vide
+
+    ---
+    """
+    #print("!! TODO : Compléter la fonction moyenne !!", end=" ")
+    #pass
+
+    if valeurs is []: 
+        raise ValueError
+    else:
+        return sum(valeurs) / len(valeurs)
+
+if __name__ == "__main__":
+    Console().print(Markdown(moyenne.__doc__))
+    print("moyenne([10, 20, 15]) :", end=" ")
+    print(moyenne([10, 20, 15]))
